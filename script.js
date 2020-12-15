f6b749a537e1252ef764cf4b9eb803bd63ccf5f5
Shoyu808 <Dexterjmp@gmail.com>
2020-12-14 22:28:53 -0800

    var

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 // TODO: Build questions for quiz
 // TODO: Setup a time interval for if someone clicks on the incorrect answer they'll lose time
 // TODO: When the user answers all the questions or timer reach 0 they'll be presented with their high score
+
+var quizBox = document.getElementById('quiz');
+var resultsBox = document.getElementById('results');
+var submitButton = document.getElementById('submit');

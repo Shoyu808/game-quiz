26f33d39361c3c22f0d0cc4ac9cb5471052e6bef
Shoyu808 <Dexterjmp@gmail.com>
2020-12-14 22:21:22 -0800

    psuedo

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//TODO: 
+//TODO: Need to create variables to target <div>
+// TODO: Create a function to build our quiz and show results to quiz
+// TODO: Build questions for quiz
+// TODO: Setup a time interval for if someone clicks on the incorrect answer they'll lose time
+// TODO: When the user answers all the questions or timer reach 0 they'll be presented with their high score

dca93c8ccabe5311e58a9d9bde178922f239d23a
Shoyu808 <Dexterjmp@gmail.com>
2020-12-15 13:48:20 -0800

    madequestions

--- script.js
+++ script.js
@@ -1,9 +1,123 @@
 //TODO: Need to create variables to target <div>
-// TODO: Create a function to build our quiz and show results to quiz
+// TODO: Create a function to build our quiz with a timer and show results to quiz
 // TODO: Build questions for quiz
 // TODO: Setup a time interval for if someone clicks on the incorrect answer they'll lose time
 // TODO: When the user answers all the questions or timer reach 0 they'll be presented with their high score
+// TODO: Create a local storage to hold high scores
+
+// My questions of arrays
+var questions = [
+    {
+        text: "When did the Super Nintendo got released?",
+        answer: [
+            "November 21, 1990",
+            "April 15, 1990",
+            "January 3, 1990",
+            "December 5, 1989"
+        ],
+        correctAnwer: 0
+    },
+    {
+        text: "Which gaming console is the best selling?",
+        answer: [
+            "Xbox",
+            "Wii",
+            "PlayStation 2",
+            "PlayStation 5"
+        ],
+        correctAnwer: 2
+    },
+    {
+        text: "Which of these games are first person shooters?",
+        answer: [
+            "Hello kitty Island Adventure",
+            "Golden eye 007",
+            "Final Fantasy",
+            "Grand theft auto"
+        ],
+        correctAnwer: 1
+    },
+    {
+        text: "What does RPG mean?",
+        answer: [
+            "Real pro gamer?",
+            "Running playing game?",
+            "Really playing games?",
+            "Role playing game?"
+        ],
+        correctAnswer: 3
+    },
+    {
+        text: "Who is the main character in Final Fantasy X?",
+        answer: [
+            "Tidus",
+            "Kimahri",
+            "Wakka",
+            "Yuna"
+        ],
+        correctAnswer: 0
+    },
+    {
+        text: "What is the main charater's name in Halo?",
+        answer: [
+            "Master Guardian",
+            "Commaning Chief",
+            "Master Chief",
+            "Commanging Guardian"
+        ],
+        correctAnswer: 2
+    },
+    {
+        text: "What is the first MMORPG?",
+        answer: [
+            "Runescape",
+            "Ultima Online",
+            "Maplestory",
+            "Realm Online"
+        ],
+        correctAnswer: 3
+    },
+    {
+        text: "What is the best selling handheld gaming console of all time?",
+        answer: [
+            "Playstation portable",
+            "Game boy color",
+            "Nintendo Switch",
+            "Nintendo DS"
+        ],
+        correctAnswer: 3
+    },
+    {
+        text: "Who invented the Xbox?",
+        answer: [
+            "Sony",
+            "Microsoft",
+            "Sega",
+            "Atari"
+        ],
+        correctAnswer: 1
+    },
+    {
+        text: "What is the best selling FPS game of all time?",
+        answer: [
+            "Counter-Strike: Global Offensive",
+            "Halo",
+            "Call of duty",
+            "Valorant"
+        ],
+        correctAnswer: 0
+    }
+];
 
 var quizBox = document.getElementById('quiz');
 var resultsBox = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var timeEl = document.getElementById('timer');
+
+function startQuiz(){};
+
+function quizQuest(){};
+
+function showResults(){}
+
+
